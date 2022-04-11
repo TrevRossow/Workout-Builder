eeaa619dc08f6c419d70e26651d0a78add75d23d
Alejandro Guerrero <Alexxxio77@gmail.com>
2022-04-11 16:17:14 -0400

    vuetify

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div id="app">
+  <v-app>
+    <v-main>
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
-    <router-view />
-  </div>
+     <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+     <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+   </div>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,11 +2,51 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
-  </div>
+    <v-app>
+    <v-carousel>
+      <v-carousel-item
+      v-for="img in caroImages"
+      :key="img.id"
+      :src="img.src"
+      >
+      </v-carousel-item>
+    </v-carousel>
+    </v-app>
+</div>
 </template>
-
 <script>
 export default {
-  name: "home"
+  name: "home",
+  data() {
+    return {
+      caroImages: [
+        {
+          id: 1,
+          src: "/Asset/Carousel/CaroAssets/istockphoto-991036038-612x612.jpg",
+          title: 'smiling guy'
+        },
+        {
+          id: 2,
+          src: "/Asset/Carousel/CaroAssets/istockphoto-1141393089-612x612.jpg",
+          title: 'ladies running'
+        },
+        {
+          id: 3,
+          src: "/Asset/Carousel/CaroAssets/istockphoto-1225596371-612x612.jpg",
+          title: 'ladies posing'
+        },
+        {
+          id: 4,
+          src: "/Asset/Carousel/CaroAssets/pexels-pixabay-38630.jpg",
+          title: 'muscly dude'
+        },
+        {
+          id: 5,
+          src: "/Asset/Carousel/CaroAssets/photo-1627483298235-f3bac2567c1c.jpg",
+          title: 'ladies stretching'
+        },
+      ],
+    };
+  },
 };
 </script>

78b2c9d54ea168443c1a0e05972a49753301b1f6
Brett Ziegler <brettziegler12@gmail.com>
2022-04-13 10:32:57 -0400

    trainer role schema

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -20,6 +20,6 @@ CREATE TABLE users (
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
-
+INSERT INTO users (username,password_hash,role) VALUES ('trainer','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_TRAINER');
 
 COMMIT TRANSACTION;

c2b96935243844851865b87b566e6ad167fbd057
Alejandro Guerrero <Alexxxio77@gmail.com>
2022-04-19 17:05:19 -0400

    date and time

--- vue/src/components/ViewSummary.vue
+++ vue/src/components/ViewSummary.vue
@@ -4,6 +4,11 @@
       <div class="timeExercising">
           This should display info
       </div>
+      <div class="currentDate" >
+          <i><font-awesome-icon icon="fa-calendar"/></i>
+          {{ date }}
+      </div>
+
       <div class="focusMostCompleted">
 
       </div>
@@ -18,6 +23,8 @@ export default {
     name: "view-summary",
     data() {
         return {
+            showDateTime: null,
+            date: null,
             workout:{}
         }
     },
@@ -25,16 +32,37 @@ export default {
     methods: {
         getWorkout(){
             this.workout = this.$store.state.summaryWorkout;
+        },
+        
+        getCurrentDatetime(){
+            let dateTime = new Date();
+            this.showDateTime = dateTime;
+            this.date = dateTime.toJSON().slice(0,10).replace(/-/g,'/');
+            console.log(dateTime);
         }
     },
 
     created() {
         this.getWorkout();
-    }
+        this.getCurrentDatetime();
+    },
+        
 
 }
+
 </script>
 
 <style>
 
+.currentDate{
+  width: 140px;
+  background-color: #3a57af;
+  border-radius: 5px;
+  padding: 8px 8px 8px 12px;
+  margin: 1rem;
+  color: white;
+  box-shadow: 2px 3px 3px rgb(58 87 175 / 75%);
+  border: solid 1px #cbc9c9;
+  position: relative;
+}
 </style>
